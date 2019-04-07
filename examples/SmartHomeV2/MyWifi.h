33e1980beb796bf62b4c060bcfeb85faaaa435e3
Gerald Lechner <lechge@gmail.com>
2019-04-07 09:24:07 +0200

    Add files via upload
    New example for ESP32 uses all new features

---
+++ examples/SmartHomeV2/MyWifi.h
@@ -0,0 +1,164 @@
+/* this subfile to ATSmartHome.ino contains allfunctions
+ *  and global variables associated to WiFi usage
+ */
+#include <WiFi.h>
+#include <esp_now.h>
+
+//Access point password and channel
+#define APPWD "123456789"
+#define APCHANNEL 0
+
+
+//NTP server and offsets for MET
+#define NTP_SERVER "de.pool.ntp.org"
+#define GMT_OFFSET_SEC 3600
+#define DAYLIGHT_OFFSET_SEC 0
+
+//Global variables
+esp_now_peer_info_t info;
+
+//init WiFi and start the access point
+void initWiFi() {
+  //ESP32 as access point and station to use Internet connection in parallel
+  //Attention !! Internet connection works only if your WLAN router uses channel 0
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP("ATSmartHome",APPWD,APCHANNEL,0);
+  return;
+}
+
+//establish a Wlan connection 
+boolean connectWlan(String ssid, String password) {
+  //pointer to the characters inside the string
+  char* txtSSID = const_cast<char*>(ssid.c_str());
+  char* txtPassword = const_cast<char*>(password.c_str());  
+  //connect to wlan
+  WiFi.begin(txtSSID, txtPassword);
+  uint8_t timeout = 0;
+  while ((WiFi.status() != WL_CONNECTED) && (timeout<10)) {
+    timeout++;
+    delay(1000);
+  }
+  //we wait max 10 s until connected
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    configTime(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+    //Aktuelle Uhrzeit ausgeben
+    Serial.println(AT_GetLocalTime());
+ }
+ return (WiFi.status() == WL_CONNECTED);
+}
+
+void printESP_Now_Err (String prefix, esp_err_t err) {
+  switch (err) {
+    case ESP_ERR_ESPNOW_BASE: Serial.println(prefix+" ESPNOW error number base"); break;
+    case ESP_ERR_ESPNOW_NOT_INIT: Serial.println(prefix+"ESPNOW is not initialized"); break;
+    case ESP_ERR_ESPNOW_ARG: Serial.println(prefix+"Invalid argument"); break;
+    case ESP_ERR_ESPNOW_NO_MEM: Serial.println(prefix+"Out of memory"); break;
+    case ESP_ERR_ESPNOW_FULL: Serial.println(prefix+" ESPNOW peer list is full"); break;
+    case ESP_ERR_ESPNOW_NOT_FOUND: Serial.println(prefix+"ESPNOW peer is not found"); break;
+    case ESP_ERR_ESPNOW_INTERNAL: Serial.println(prefix+"Internal error"); break;
+    case ESP_ERR_ESPNOW_EXIST: Serial.println(prefix+"ESPNOW peer has existed"); break;
+    case ESP_ERR_ESPNOW_IF: Serial.println(prefix+"Interface error"); break;
+    default : Serial.println(prefix+"Unknown error"); break;
+  }
+}
+
+
+//add a new peer to the peer list
+esp_err_t addPeer(const uint8_t mac[6]) {
+  esp_err_t res = ESP_OK;
+  if (!esp_now_is_peer_exist(mac)) {
+    for (int ii=0;ii<6;ii++) info.peer_addr[ii] = mac[ii];
+    info.channel = 0;
+    info.encrypt = 0;
+    const esp_now_peer_info_t *peer = &info;
+    res = esp_now_add_peer(peer);
+    if (res != ESP_OK) printESP_Now_Err("Add Peer ",res);
+  }
+  return res;
+} 
+
+
+// callback for ESP Now
+void readESPNow(const uint8_t *mac_addr, const uint8_t *r_data, int data_len) {
+  int16_t devnr;
+  uint8_t pkts;
+  String strid;
+  uint8_t sz;
+  uint8_t buf[256];
+  int8_t packets;
+  esp_err_t err;
+  ATMESSAGEHEADER msghdr;
+  ATDATAPACKET dp;
+  //r_data points on to the received data 
+  msg.readBuffer(r_data);
+  msghdr = msg.getHeader();
+  strid = AT_GetId(msghdr.id);
+  //look into database if device is known
+  devnr = database.findDevice(msghdr.id);
+  Serial.print("Got message from ");
+  Serial.print(strid);
+  Serial.print(" = device ");
+  Serial.println(devnr);
+  if (devnr < 0) {
+    //unknown device we save it as new device
+    database.readNewMessage(strid,r_data);
+    dsp.setStatus(strid);
+  } else {
+    //know device we update the results
+    pkts = msg.getPackets();
+    for (uint8_t i = 0; i< pkts; i++) {
+      dp = msg.getData(i);
+      database.setResult(devnr * ATMAXDEVCHANNELS + dp.channel, dp);
+    }
+    //we look if there is something to answer
+    sz = 250; //maximum payload for esp now 
+    packets = database.getResponse(devnr,&buf[0],&sz);
+    if (packets>0) {
+      err = addPeer(mac_addr);
+      if (err == ESP_OK) {
+        err = esp_now_send(mac_addr, buf, sz);
+        if (err != ESP_OK) printESP_Now_Err("Send Data ",err);
+      }
+    }
+  }
+  msg.clear();
+}
+
+//send data to an esp-device
+//todo add usage for other outputs too currentrly only switch
+void sendData(uint16_t index) {
+  uint8_t dev = index/ATMAXDEVCHAN;
+  String id;
+  esp_err_t err;
+  id = database.getDeviceId(dev);
+  if (id != "") {
+    Serial.println("Send Data");
+    AT_MessageBuffer sendbuf;
+    ATCURVALUES val;
+    uint8_t buf[100];
+    uint8_t sz = 100;
+    uint8_t ch = index - (dev * ATMAXDEVCHAN);
+    sendbuf.clear();
+    sendbuf.setId(id);
+    val = database.getResult(index);
+    sendbuf.addSwitchOut(val.value[0]!=0, ch);
+    sendbuf.fillBuffer(buf,&sz);
+    err = addPeer(buf);
+    if (err == ESP_OK) {
+      err = esp_now_send(buf, buf, sz);
+      if (err != ESP_OK) printESP_Now_Err("Send Data ",err);
+    }
+  }
+}
+
+//start ESP NOW
+boolean initESPNow() {
+  esp_now_deinit();
+  boolean result = (esp_now_init() == ESP_OK);
+  info.channel = APCHANNEL;
+  info.encrypt = 0;
+  if (result) esp_now_register_recv_cb(readESPNow);
+  return result;
+}
--- examples/SmartHomeV2/SmartHomeV2.ino
+++ examples/SmartHomeV2/SmartHomeV2.ino
@@ -1,1 +1,91 @@
 
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <XPT2046_Touchscreen.h>
+#include "TouchEvent.h"
+#include "FS.h"
+#include "SPIFFS.h"
+//ArduiTouch libraries by Gerald Lechner from github
+#include "AT_Database.h"
+#include "AT_MessageBuffer.h"
+#include "TouchEvent.h"
+#include <AT_Display.h>
+
+
+//used pins
+#define TFT_CS   5      //diplay chip select
+#define TFT_DC   4      //display d/c
+#define TFT_RST  22     //display reset
+#define TFT_LED  15     //display background LED
+
+#define TOUCH_CS 14     //touch screen chip select
+#define TOUCH_IRQ 2     //touch screen interrupt
+
+//file names used with SPIFFS do not remove the starting slash!
+
+#define ATDEVICEFILE "/devices.txt"
+#define ATCONFIGFILE "/config.txt"
+
+//data structures for database and message buffer
+AT_Database database(ATDEVICEFILE,ATCONFIGFILE);
+AT_MessageBuffer msg;
+Adafruit_ILI9341 tft(TFT_CS,TFT_DC,TFT_RST);
+XPT2046_Touchscreen touch(TOUCH_CS,TOUCH_IRQ);
+TouchEvent tevent(touch);
+
+AT_Display dsp(&tft,&database,TFT_LED);
+
+//Shoud be defined if a Ardui-Touch version 01-02 or higher is used
+//#define ARDUITOUCHB
+
+
+//sub files for this scetch do not change the order
+#include "MyWifi.h"     //all associated to WiFi
+
+//Format flash file system if not already done
+#define FORMAT_SPIFFS_IF_FAILED true
+
+void onClick(TS_Point p) {
+  Serial.println("Click");
+  dsp.onClick(p);
+}
+void onLong(TS_Point p) {
+  dsp.onLongClick(p);
+}
+void onSwipe(uint8_t direction) {
+  dsp.onSwipe(direction);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); 
+  Serial.println("Start");
+  pinMode(TFT_LED,OUTPUT);
+  int heap = ESP.getFreeHeap();
+  Serial.printf("RAM available %i\n",heap);
+  touch.begin();
+  tevent.setResolution(tft.width(),tft.height());
+  tevent.setDblClick(200);
+  tevent.registerOnTouchLong(onLong);
+  tevent.registerOnTouchClick(onClick);
+  tevent.registerOnTouchSwipe(onSwipe);
+  dsp.begin(1);
+  dsp.display(true);
+  dsp.registerOnResultChange(sendData);
+  //start flash file system
+  if (SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) Serial.println(F("SPIFFS loaded"));
+  initWiFi();
+  //connectWlan("","");
+  delay(100);
+  if (!initESPNow()) dsp.setStatus("ESP NOW failed");
+  
+  if (!database.readDevices()) Serial.println("Can not read devices");
+  if (!database.readConfig()) Serial.println("Can not read widget config");
+  dsp.showCurrentPage();
+
+}
+
+void loop() {
+  tevent.pollTouchScreen();
+  dsp.updateDisplay();
+}
